7f216982f3088de4a5df40a334a2e9cdcf73488b
Clare Pinder <Clare.pinder@live.co.uk>
2019-03-05 15:51:48 +0000

    Finished!

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,11 +1,25 @@
 "use strict";
-// describe("FizzBuzz", function(){
-//     var fizzBuzz;
-// })
 
 describe('FizzBuzz', function() {
-    it('Says Fizzbuzz is divisible by 15', function() {
-      var fizzBuzz = new FizzBuzz()
+
+  beforeEach(function() {
+    var fizzBuzz = new FizzBuzz()
+  });
+
+  it('Says Fizzbuzz is divisible by 15', function() {
     expect(fizzBuzz.play(15)).toEqual('Fizzbuzz');
   });
+
+  it('Says Fizz if divisible by 3', function() {
+    expect(fizzBuzz.play(3)).toEqual('Fizz');
+  });
+
+  it('Says Buzz if divisible by 5', function() {
+    expect(fizzBuzz.play(5)).toEqual('Buzz');
+  });
+
+  it('Says Buzz if divisible by 5', function() {
+    expect(fizzBuzz.play(7)).toEqual(7);
+  });
+
 });
